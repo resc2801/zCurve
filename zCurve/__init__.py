ff184a19629f4e377e9288190997970cbdb5f331
rmrschub <rene.schubotz@dfki.de>
2021-05-18 22:41:55 +0200

    version increment

--- zCurve/__init__.py
+++ zCurve/__init__.py
@@ -3,7 +3,7 @@ zCurve.
 zCurve maps multidimensional data to one dimension while preserving locality of the data points.
 """
 
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 __author__ = 'René Schubotz'
 __credits__ = 'German Research Centre for Artificial Intelligence'
 
